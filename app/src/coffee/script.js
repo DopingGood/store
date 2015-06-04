929d8f2ed531b6a2e7517eb5d59f60fe5bc784fa
DopingGood <nektomakseem@yandex.ru>
2015-06-04 22:01:34 +0400

    first try

---
+++ app/src/coffee/script.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+	$(".img-slicker").slick({
+		nextArrow: $(".right-arrow"),
+		prevArrow: $(".left-arrow"),
+		swipe: false,
+		touchMove: true,
+		slidesToShow: 6,
+		respondTo: "slider"
+	});
+	// nav-bar bascket
+	$(".bascket, .bascket-dropdown").mouseover(function(){
+		$(".bascket").addClass("bascket-hovered");
+		$(".bascket-dropdown").show();
+	});
+	$(".bascket, .bascket-dropdown").mouseleave(function(){
+		$(".bascket").removeClass("bascket-hovered");
+		$(".bascket-dropdown").hide();
+	});
+	// nav-bar seeker
+	$(".seeker").click(function(){
+		if ($(".seeker").hasClass("hovered-seeker") == false) {
+				$(".seeker").addClass("hovered-seeker");
+				$(".seeker-text").show("slide", {direction: "right"}, 120);
+				$(".seeker-text").focus();
+		}
+		else {
+			$(".seeker").removeClass("hovered-seeker");
+			$(".seeker-text").hide("slide", {direction: "right"}, 120);
+		};
+	});
+	$('.selectpicker').selectpicker({
+		dropupAuto: false,
+		size: 3
+	});
+
+	$('.filter-select').selectpicker({
+		dropupAuto: false,
+		size: 5
+	});
+
+	$('.mobile-filter-select').selectpicker({
+		dropupAuto: false,
+		size: 4
+	})
+
+	$('.cart-selectpicker').selectpicker({
+		dropupAuto: false,
+		size: 4
+	});
+
+	$('.selectpicker-cards').selectpicker({
+		dropupAuto: false,
+		size: 2
+	});
+
+	$('.country-select').selectpicker({
+		dropupAuto: false,
+		size: 5
+	});
+	// Modal window
+	$('#myModal').on('shown.bs.modal', function () {
+	$('#myInput').focus()
+	});
+	//Password
+	$(".eye-toggle").addClass("ciphering");
+	var $showSymbol = function (value){
+		$(".login-password").attr("type", value);
+		$(".eye-toggle").toggleClass("ciphering");
+		if (value == "password") {
+			console.log("Пароль функционирует");
+		};
+	};
+	$(".eye-toggle").click(function(){
+		if ($(".eye-toggle").hasClass("ciphering") == true) {
+			$showSymbol("text");
+		}
+		else {
+			$showSymbol("password");
+		};
+	});
+	//Subtotal count
+	var Subtotal = 0;
+	$(".shopping-price").each(function(){
+		Subtotal += parseFloat($(this).text());
+		console.log(Subtotal);
+	});
+	$(".shopping-subtotal").text(Subtotal.toFixed(2));
+});
